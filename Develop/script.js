b9ae69af03c91e8612190888b04908534ef4538b
Michael Cui <cuimichael500@gmail.com>
2020-06-15 13:54:21 -0500

    Got button to function with first couple of prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var passLength = confirm("Do you want to include password length criteria?");
+  var passContent = confirm("Do you want to include password content criteria?");
+
+  if(passLength){
+
+    var selectedLength = prompt("How long should your password be? (Limited between 18-128 characters");
+  }
+  
+  
+  if(passContent){
+
+    var selectedContent = confirm("Do you want to include lower case letters?")
+  }
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
