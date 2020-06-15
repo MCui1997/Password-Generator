50581ec78cfb52d033691bafb70c1b0bf63d4484
Michael Cui <cuimichael500@gmail.com>
2020-06-15 14:10:11 -0500

    Added comments

--- Develop/script.js
+++ Develop/script.js
@@ -7,20 +7,24 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
 
+// Ask two confirm prompts to determine which criteria should be included
   var passLength = confirm("Do you want to include password length criteria?");
   var passContent = confirm("Do you want to include password content criteria?");
 
 
+  // If neither criteria is selected, alert that no password could be generated
   if(!passLength && !passContent){
     alert("No password could be generated.");
   }
 
 
+  // If length was selected as criteria, then ask how many characters should be in the password
   if(passLength){
     var selectedLength = prompt("How long should your password be? (Limited between 18-128 characters");
   }
   
   
+  // If content was selected, then ask what types of characters the user wants
   if(passContent){
 
     var ifLowerCase = confirm("Do you want to include lower case letters?")
@@ -28,6 +32,8 @@ function writePassword() {
     var ifNumeric = confirm("Do you want to include numbers?")
     var ifSpecial = confirm("Do you want to include special characters?")
   }
+
+
 
 }
 
